b2df69153f6a628f33d7b1ddc7ed5abaf1e470e2
Smander <pris.smss87@gmail.com>
2026-03-08 17:36:51 +0000

    API_WORKING

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from "cors"
 
 import { PrismaClient } from '@prisma/client'
 
@@ -6,6 +7,7 @@ const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 
 app.post("/users", async (req, res) => {
