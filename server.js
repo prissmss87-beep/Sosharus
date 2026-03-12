53e06c8e4b22517c226161572aa7ab4833a216bb
Smander <pris.smss87@gmail.com>
2026-03-12 15:39:29 +0000

    Backend almost

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import cors from "cors"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import 'dotenv/config'
 
 import { PrismaClient } from '@prisma/client'
 
@@ -9,34 +12,76 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body
+
+    
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email: email }
+        })
+
+        if (!user) {
+            return res.status(404).json({ message: "Usuário não encontrado" })
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password)
+        if (!passwordMatch) {
+            return res.status(401).json({ message: "Senha incorreta" })
+        }
+
+        const token = jwt.sign(
+            { id: user.id, email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: "1h"}
+        )
+
+        res.status(200).json({ token, user: { id: user.id, name: user.name, email: user.email } })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: "Erro no servidor" })
+    }
+})
 
 app.post("/users", async (req, res) => {
-    
-    await prisma.user.create({
-        data: {
-            name: req.body.name,
-            email: req.body.email,
-            age: req.body.age
-        }
-    })
 
-    res.status(201).json(req.body)
+    try {
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        console.log(salt)
+        console.log(hashedPassword)
+
+        await prisma.user.create({
+            data: {
+                name: req.body.name,
+                email: req.body.email,
+                age: req.body.age,
+                password: hashedPassword
+            }
+        })
+        res.status(201).json(req.body)
+    }
+    catch{
+        res.status(500).send()
+    }
 })
 
 
 app.put("/users/:id", async (req, res) => {
-    
+
     console.log(req)
 
 
-        await prisma.user.update({
+    await prisma.user.update({
         where: {
             id: req.params.id
         },
         data: {
             name: req.body.name,
             email: req.body.email,
-            age: req.body.age
+            age: req.body.age,
+            password: req.body.password
         }
     })
 
@@ -49,12 +94,13 @@ app.get("/users", async (req, res) => {
 
     let users = []
 
-    if(req.query){
+    if (req.query) {
         users = await prisma.user.findMany({
             where: {
                 name: req.query.name,
                 email: req.query.email,
-                age: req.query.age
+                age: req.query.age,
+                password: req.query.password
             }
         })
     } else {
@@ -65,15 +111,15 @@ app.get("/users", async (req, res) => {
     res.status(200).json(users)
 })
 
-app.delete("/users/:id", async (req,res) => {
+app.delete("/users/:id", async (req, res) => {
 
     await prisma.user.delete({
-        where:{
+        where: {
             id: req.params.id
         }
     })
 
-    res.status(200).json({message: "Usuario deletado"})
+    res.status(200).json({ message: "Usuario deletado" })
 })
 
 
